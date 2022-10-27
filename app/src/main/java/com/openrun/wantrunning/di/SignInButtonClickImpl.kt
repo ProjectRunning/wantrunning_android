3b10130f04216d3e080acd33d1f20bc23f35f15a
wooyoung <djdj7087@gmail.com>
2022-10-27 22:51:02 +0900

    dependency 정리 및 로그인 메소드 수정

--- app/src/main/java/com/openrun/wantrunning/di/SignInButtonClickImpl.kt
+++ app/src/main/java/com/openrun/wantrunning/di/SignInButtonClickImpl.kt
@@ -1,7 +1,15 @@
 package com.openrun.wantrunning.di
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.kakao.sdk.user.UserApiClient
 import com.openrun.wantrunning.R
 import com.openrun.wantrunning.feature.landing.signin.SignInButtonClick
 import javax.inject.Inject
@@ -11,7 +19,49 @@ class SignInButtonClickImpl @Inject constructor(
 ) : SignInButtonClick {
 
     override fun kakaoSignIn() {
+//        activity.signInWithKakao()
         activity.findNavController(R.id.fcv_main_nav_host)
             .navigate(R.id.action_landingNavHostFragment_to_mainFeatureFragment)
+    }
+
+    override fun googleSignIn(activityResultLauncher: ActivityResultLauncher<Intent>) {
+        activity.signInWithGoogle(launcher = activityResultLauncher)
+    }
+
+    override fun onGoogleSignInResult(activityResult: ActivityResult) {
+        if (activityResult.resultCode == Activity.RESULT_OK) {
+            Log.d("SignInButtonClickImpl", "onGoogleSignInResult: ${activityResult.data?.extras}")
+        } else {
+            Log.d("SignInButtonClickImpl", "onGoogleSignInResult: ${activityResult.data}")
+        }
+    }
+
+    private fun Activity.signInWithKakao() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            signInWithKakaoTalk()
+        } else {
+            signInWithKakaoAccount()
+        }
+    }
+
+    private fun Activity.signInWithKakaoTalk() {
+        UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+            Log.d("SignInFragment", "signInWithKakaoTalk: $token, $error")
+        }
+    }
+
+    private fun Activity.signInWithKakaoAccount() {
+        UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+            Log.d("SignInFragment", "signInWithKakaoAccount: $token, $error")
+        }
+    }
+
+    private fun Activity.signInWithGoogle(launcher: ActivityResultLauncher<Intent>) {
+        val googleSignInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        launcher.launch(googleSignInClient.signInIntent)
     }
 }
--- feature/landing/src/main/java/com/openrun/wantrunning/feature/landing/signin/SignInFragment.kt
+++ feature/landing/src/main/java/com/openrun/wantrunning/feature/landing/signin/SignInFragment.kt
@@ -2,15 +2,11 @@ package com.openrun.wantrunning.feature.landing.signin
 
 import android.app.Activity.RESULT_OK
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.kakao.sdk.user.UserApiClient
 import com.openrun.wantrunning.feature.landing.databinding.FragmentSignInBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -27,11 +23,7 @@ class SignInFragment : Fragment() {
     private val googleSignInRequestLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { activityResult ->
-        if (activityResult.resultCode == RESULT_OK) {
-            Log.d("SignInFragment", "google sign in complete: ${activityResult.data?.extras}")
-        } else {
-            Log.d("SignInFragment", "google sign in failed: ${activityResult.data}")
-        }
+        signInButtonClick.onGoogleSignInResult(activityResult)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -47,36 +39,7 @@ class SignInFragment : Fragment() {
         }
 
         binding.btnSignInGoogle.setOnClickListener {
-            signInWithGoogle()
+            signInButtonClick.googleSignIn(activityResultLauncher = googleSignInRequestLauncher)
         }
-    }
-
-    private fun signInWithKakao() {
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
-            signInWithKakaoTalk()
-        } else {
-            signInWithKakaoAccount()
-        }
-    }
-
-    private fun signInWithKakaoTalk() {
-        UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
-            Log.d("SignInFragment", "signInWithKakaoTalk: $token, $error")
-        }
-    }
-
-    private fun signInWithKakaoAccount() {
-        UserApiClient.instance.loginWithKakaoAccount(requireContext()) { token, error ->
-            Log.d("SignInFragment", "signInWithKakaoAccount: $token, $error")
-        }
-    }
-
-    private fun signInWithGoogle() {
-        val googleSignInOptions = GoogleSignInOptions
-            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
-        val googleSignInClient = GoogleSignIn.getClient(requireContext(), googleSignInOptions)
-        googleSignInRequestLauncher.launch(googleSignInClient.signInIntent)
     }
 }
